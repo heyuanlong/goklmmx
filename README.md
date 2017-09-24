ff413c50eb91838984911d7f425e16e94511394b
heyuanlong <1023417614@qq.com>
2017-09-25 00:07:37 +0800

    t

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # goklmmx
 goklmmx
+
+
+###依赖的包：
+* github.com/golang/protobuf/
+* github.com/robfig/config
+* github.com/parnurzeal/gorequest
--- work/center/main.go
+++ work/center/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"net"
+	klog "goklmmx/lib/log"
+	kconf "goklmmx/lib/conf"
+)
+
 func main() {
+	kconf.SetFile("conf/config.cfg")
+	klog.SetLogfile()
+
+
+	c, _ := kconf.GetConf()
+	serverPort,_ := c.String("server","port")
+	listen_sock,err := net.Listen("tcp",":"+serverPort)
+	if err != nil{
+		klog.Klog.Fatalln(err)
+	}
+	defer listen_sock.Close()
+	for{
+		_,err := listen_sock.Accept()
+		if err != nil {
+			klog.Klog.Println("listen_sock.Accept error:",err)
+			continue
+		}
+		klog.Klog.Println("kkkkkkk")
+	}
+
 
 }
